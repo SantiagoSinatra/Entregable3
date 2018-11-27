782d3967c5dbb99ff950750b36b0847b9c7659ec
Santi <santiagosinatra@gmail.com>
2018-11-27 17:34:34 -0300

    Login con Facebook
    11% Metodos de compra

--- app/src/main/java/com/dhsantiagosinatra/momaapp/view/MainActivity.java
+++ app/src/main/java/com/dhsantiagosinatra/momaapp/view/MainActivity.java
@@ -1,11 +1,20 @@
 package com.dhsantiagosinatra.momaapp.view;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 import com.dhsantiagosinatra.momaapp.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
+
 }
